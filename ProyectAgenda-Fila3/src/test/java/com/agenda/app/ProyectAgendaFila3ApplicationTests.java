feaaab7d0abf5e5e66c5b89a7332135d057fcdf9
AnaMariaFer <45061776+AnaMariaFer@users.noreply.github.com>
2018-12-05 15:21:03 +0100

    Add files via upload

---
+++ ProyectAgenda-Fila3/src/test/java/com/agenda/app/ProyectAgendaFila3ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.agenda.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProyectAgendaFila3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
